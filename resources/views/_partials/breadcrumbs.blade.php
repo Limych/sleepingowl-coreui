839d37d446a6b54b9825bc888ff3e3bc12c30ade
Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
2017-10-05 11:58:17 +0300

    Remaked Breadcrumbs and Badges

---
+++ resources/views/_partials/breadcrumbs.blade.php
@@ -0,0 +1,13 @@
+@if ($breadcrumbs)
+    <ol class="breadcrumb">
+        @foreach ($breadcrumbs as $breadcrumb)
+            @if($breadcrumb->last)
+                <li class="breadcrumb-item active">{{{ $breadcrumb->title }}}</li>
+            @elseif ($breadcrumb->url && ($breadcrumb->url !== '#'))
+                <li class="breadcrumb-item"><a href="{{{ $breadcrumb->url }}}">{{{ $breadcrumb->title }}}</a></li>
+            @else
+                <li class="breadcrumb-item">{{{ $breadcrumb->title }}}</li>
+            @endif
+        @endforeach
+    </ol>
+@endif
---
+++ src/Navigation/Badge.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright (c) 2017 Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
+ */
+
+namespace Limych\SleepingOwlCoreUI\Navigation;
+
+class Badge extends \SleepingOwl\Admin\Navigation\Badge
+{
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $value = $this->getValue();
+
+        if (! $this->hasClassProperty('badge-', 'bg-')) {
+            $this->setHtmlAttribute('class', 'badge badge-primary');
+        }
+
+        return [
+            'value'      => $value,
+            'attributes' => $this->htmlAttributesToString(),
+        ];
+    }
+}
